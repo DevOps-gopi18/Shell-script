1df51868b0ab6cf0d63ae83aba4ef885b75e5b0b
gopih <gopi47590@gmail.com>
2025-01-28 15:42:10 +0530

    conversation ended

--- 02-variables.sh
+++ 02-variables.sh
@@ -5,3 +5,5 @@ echo "Ramesh:: Hello Suresh, how are you?"
 echo "Suresh:: Hello Ramesh, i am good. How are you?"
 echo "Ramesh:: I am super! have you finished devops practice today Suresh?"
 echo "Suresh:: No Ramesh, i have some urgent work outside today"
+
+echo "conversation ended!"
