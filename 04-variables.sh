aeacf14b332f5d1de663f54367b3c41bf4325000
gopih <gopi47590@gmail.com>
2025-01-28 16:13:08 +0530

    intoduction is added

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+# conversation between two freinds!!
+echo "$PERSON1:: Hello $PERSON2, how are you?"
+echo "$PERSON2:: Hello $PERSON1, i am good. How are you?"
+echo "$PERSON1:: I am super! have you finished devops practice today $PERSON2?"
+echo "$PERSON2:: No $PERSON1, i have some urgent work outside today"
+
+echo "conversation ended!"
