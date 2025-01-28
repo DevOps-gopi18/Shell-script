ebdd73a698d7251a7c8f0a11deba507beca05fb8
gopih <gopi47590@gmail.com>
2025-01-28 15:29:38 +0530

    2 friends conversation is added

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# conversation between two freinds!!
+echo "Ramesh:: Hello Suresh, how are you?"
+echo "Suresh:: Hello Ramesh, i am good. How are you?"
+echo "Ramesh:: I am super! have you finished devops practice today Suresh?"
+echo "Suresh:: No Ramesh, i have some urgent work outside today"
