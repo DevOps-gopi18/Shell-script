bc824355de0b0f811f8b646b1f6dfa3cf0a45c8d
gopih <gopi47590@gmail.com>
2025-01-29 12:47:44 +0530

    checking the script time

--- 06-data-type.sh
+++ 06-data-type.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+TIMESTAMP=$(date) #time variable
 
-echo "script executed at :$TIMESTAMP"
+echo "script executed at :$TIMESTAMP" # printing the time
+
+#adding 2 numbers
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$((NUMBER1+NUMBER2)) #ADDING NUMBERS
