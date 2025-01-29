232175952e2f65bfb3b0ee91d884d168fb048b65
gopih <gopi47590@gmail.com>
2025-01-29 12:56:20 +0530

    checking the script time

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -3,3 +3,7 @@
 MOVIE=("PUSHPA" "RRR" "DEVARA")
 
 echo "first movie is :${MOVIE[0]}"
+echo "second movie is :${MOVIE[1]}"
+echo "third movie is :${MOVIE[2]}"
+
+echo "All the movie names : ${MOVIE[@]}"
