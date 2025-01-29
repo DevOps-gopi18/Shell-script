398b3bb50e6de70283fa87019b9fa30d5adbe617
gopih <gopi47590@gmail.com>
2025-01-29 13:07:04 +0530

    checking the script time

---
+++ 09-conditions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# -gt -lt -eq -ge -le
+# -gt = greater than
+# -lt = less than
+# -eq = less than or equal
+# -ge = greater than or equal
+# -le = less than or equal
+
+#Given number is greater than(100)?
+
+NUMBER=$1
+
+if [ NUMBER -gt 100 ]
+than
+    echo "Given number is grater than: $NUMBER"
+else
+    echo "Given number is less than: $NUMBER"
+fi
