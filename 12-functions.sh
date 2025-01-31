81208bcd63fe8f3b689cb906b6b90d52d5e7ae69
gopih <gopi47590@gmail.com>
2025-01-31 11:27:46 +0530

    checking the script time

---
+++ 12-functions.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Git instllation...
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 installation ...FAILED"
+        exit 1
+    else
+        echo "$2 installtion ...SUCCESS!"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR : You must have root access to execute the program"
+    exit 1
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else
+    echo "GIT already ...installed"
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? "Installing MySql"
+else
+    echo "mysql already ...installed"
+fi
+
