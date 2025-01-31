dc375bdc2d914eff67cd95c80481cbd9b7c5db1d
gopih <gopi47590@gmail.com>
2025-01-31 11:40:44 +0530

    checking the script time

--- 12-functions.sh
+++ 12-functions.sh
@@ -19,11 +19,11 @@ then
     exit 1
 fi
 
-dnf list installed git
+dnf list installed gitt
 
 if [ $? -ne 0 ]
 then
-    dnf install git -y
+    dnf install gitt -y
     VALIDATE $? "Installing Git"
 else
     echo "GIT already ...installed"
