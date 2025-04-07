9289d8a6a63b766a653c1fff5c62134fbdfc868c
gopih <gopi47590@gmail.com>
2025-04-07 12:51:14 +0530

    adding test

--- test.sh
+++ test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-PERSON1=$1
-PERSON2=$2
+TIMESTAMP=$(date)
 
-#Conversation b/t two friends
-echo "$PERSON1: Hi, how are you $PERSON2?"
-echo "$PERSON2: Hi, I am doing great, how about you $PERSION1?" 
+echo "print the time... $TIMESTAMP"
