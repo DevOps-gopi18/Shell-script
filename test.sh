3a539b35eb9929fd46bce24e284feefb8fb07281
gopih <gopi47590@gmail.com>
2025-04-07 12:58:24 +0530

    adding test

--- test.sh
+++ test.sh
@@ -12,3 +12,11 @@ NUMBER2=$2
 SUM=$((NUMBER1*NUMBER2))
 
 echo "The values are....$SUM"
+
+CINEMA={Devara,Pushpa,RRR}
+
+echo "First occurence of the movie is $CINEMA[0]"
+echo "Second occurence of the movie is $CINEMA[1]"
+echo "Third occurence of the movie is $CINEMA[2]"
+
+echo "All occureses...$CINEMA[@]"
