77aace6c9fc00e56f059b636ded0ff96bf4cd9a9
gopih <gopi47590@gmail.com>
2025-04-07 13:08:07 +0530

    adding test

--- test.sh
+++ test.sh
@@ -6,17 +6,26 @@ echo "print the time... $TIMESTAMP"
 
 #multiplying 2 numbers
 
-NUMBER1=$1
-NUMBER2=$2
+# NUMBER1=$1
+# NUMBER2=$2
 
-SUM=$((NUMBER1*NUMBER2))
+# SUM=$((NUMBER1*NUMBER2))
 
-echo "The values are....$SUM"
+# echo "The values are....$SUM"
 
-CINEMA=("Devara" "Pushpa" "RRR")
+# CINEMA=("Devara" "Pushpa" "RRR")
 
-echo "First occurence of the movie is ${CINEMA[0]}"
-echo "Second occurence of the movie is ${CINEMA[1]}"
-echo "Third occurence of the movie is ${CINEMA[2]}"
+# echo "First occurence of the movie is ${CINEMA[0]}"
+# echo "Second occurence of the movie is ${CINEMA[1]}"
+# echo "Third occurence of the movie is ${CINEMA[2]}"
 
-echo "All occureses...${CINEMA[@]}"
+# echo "All occureses...${CINEMA[@]}"
+
+#Given number is greater than(100)?
+NUMBER=$1
+
+if [ $NUMBER -gt 100 ]
+then
+    echo "given number is greater than 100"
+else
+    echo "given number is less than 100"
