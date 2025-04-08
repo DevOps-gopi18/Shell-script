52b91d7c1b472cf846abf7eee4121a708aeb9344
gopih <gopi47590@gmail.com>
2025-04-08 11:51:57 +0530

    adding test

--- test.sh
+++ test.sh
@@ -146,7 +146,7 @@ while read -r line
 do
     USAGE=$( echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$( echo $line | awk -F " " '{print $NF}')
-    if [ $USAGE -ge $PARTITION ]
+    if [ $USAGE -ge $DISK_THRESHOLD ]
     then
         MSG+="High disk usage on partition: $PARTITION usage is: $USAGE \n"
     fi
