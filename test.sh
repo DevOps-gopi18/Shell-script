7f526c2c891ba0111ec8a9622066904a9879b366
gopih <gopi47590@gmail.com>
2025-04-08 11:48:47 +0530

    adding test

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-TIMESTAMP=$(date)
+# TIMESTAMP=$(date)
 
-echo "print the time... $TIMESTAMP"
+# echo "print the time... $TIMESTAMP"
 
 #multiplying 2 numbers
 
@@ -59,46 +59,46 @@ echo "print the time... $TIMESTAMP"
 
 
 #installing GIT
-USERID=$(id -u)
+# USERID=$(id -u)
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-W="\e[0m"
-B="\e[44m"
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# W="\e[0m"
+# B="\e[44m"
 
-#log
-# LOG_FOLDER="/var/log/shell-log"
-# FILE_NAME=$( echo $0 | cut -d "." -f1 )
-# TIMESTAMP=$( date +%Y-%m-%D-%H-%M-%S )
-# LOG_FILE_NAME="$LOG_FOLDER/$FILE_NAME-$TIMESTAMP.log"
+# #log
+# # LOG_FOLDER="/var/log/shell-log"
+# # FILE_NAME=$( echo $0 | cut -d "." -f1 )
+# # TIMESTAMP=$( date +%Y-%m-%D-%H-%M-%S )
+# # LOG_FILE_NAME="$LOG_FOLDER/$FILE_NAME-$TIMESTAMP.log"
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 installation ...$R FAILED $W"
-        exit 1
-    else
-        echo -e "$2 installtion ...$G SUCCESS! $W"
-    fi
-}
+# VALIDATE(){
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e "$2 installation ...$R FAILED $W"
+#         exit 1
+#     else
+#         echo -e "$2 installtion ...$G SUCCESS! $W"
+#     fi
+# }
 
-if [ $USERID -ne 0 ]
-then
-    echo "You must have root access to execute the program..ERROR"
-fi
+# if [ $USERID -ne 0 ]
+# then
+#     echo "You must have root access to execute the program..ERROR"
+# fi
 
-for package in $@
-do
-    dnf list installed $package 
-    if [ $? -ne 0 ]
-    then
-        dnf install $package -y 
-        VALIDATE $? "installing $package"
-    else
-        echo -e "$Y $package is already...INSTALLED $W"
-    fi
-done
+# for package in $@
+# do
+#     dnf list installed $package 
+#     if [ $? -ne 0 ]
+#     then
+#         dnf install $package -y 
+#         VALIDATE $? "installing $package"
+#     else
+#         echo -e "$Y $package is already...INSTALLED $W"
+#     fi
+# done
 
 #Install Nginx
 
@@ -135,3 +135,22 @@ done
 # else
 #     echo -e "$Y Nginx is already...INSTALLED $W"
 # fi
+
+#DISK USAGE
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5
+MSG=""
+
+while read -r line
+do
+    USAGE=$( echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$( echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $PARTITION ]
+    then
+        MSG+="High disk usage on partition: $PARTITION usage is: $USAGE \n"
+    fi
+
+done <<< $DISK_USAGE
+
+echo -e "Message : $MSG"
